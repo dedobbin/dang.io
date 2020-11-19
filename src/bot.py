ef5fe0c5dee73bbfe2d95d4a12c80465ef45a832
debobbin <you@example.com>
2020-11-19 07:49:03 +0100

    wip

--- src/bot.py
+++ src/bot.py
@@ -1,15 +1,16 @@
-import os, json, sys, string, csv, datetime
+import os, json, sys, string, csv, datetime, re
 from random import choice, randrange
 import discord as discord_api
 from discord.ext import commands
 import inspect
 from youtube import Youtube, YoutubeChannel
 from dang_error import DangError
-from helpers import debug_print, env
+from helpers import debug_print, env, get_text
 
 DISCORD_TOKEN = env('DISCORD_TOKEN')
 DISCORD_GUILD = env('DISCORD_GUILD')
 QUOTES_FILE = env('QUOTES_FILE')
+EMOJI_MAP_FILE = env('EMOJI_MAP')
 
 
 d,m,y = env('DEFAULT_CHANNEL_FIRST_UPLOAD_DATE').split('-')
@@ -19,6 +20,9 @@ default_channel = YoutubeChannel(env('DEFAULT_CHANNEL_ID'),first_upload_date)
 bot = commands.Bot(command_prefix='!')
 bot.add_cog(Youtube(bot, default_channel))
 guild = None
+
+# Used to translate ___EMOJI_CRY___ etc from string to guild emoji, mapping should be stored in EMOJI_MAP_FILE
+emoji_map = None
 
 def get_random_quote():
 	poetic_quotes = []
@@ -38,11 +42,25 @@ def get_emoji(name):
 	return ''
 
 def magic_eight_ball():
-	return choice([
-		'hmmm', 
-		get_emoji('dangs_up2'),
-		get_emoji('uh'),
-	])
+	return choice(get_text('magic_eight_ball'))
+
+# Replace ___EMOJI_CRY___ with proper emoji, based on emoji_map, fallback is regular emojis
+def parse_str_emoji(input):
+	global emoji_map
+	if not emoji_map:
+		with open(EMOJI_MAP_FILE) as f:
+			emoji_map = json.load(f)
+	
+	# ___EMOJI_.*___
+	p = re.compile(r"(.*)(___EMOJI_.*___)(.*)")
+	for e in re.findall(p, input):
+		print(e)
+		
+
+
+
+	return input
+
 
 @bot.command(name='mooi')
 async def send_quote(ctx):
@@ -53,7 +71,7 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandInvokeError) and isinstance(error.original, DangError):
         await ctx.send(str(error.original) + ' ' + get_emoji('cry'))
     else:
-        await ctx.send('er is iets niet goed gegaan ' + get_emoji('cry'))
+        await ctx.send(get_text(error))
 		# TODO: raise
 
 @bot.event
@@ -80,9 +98,10 @@ async def on_message(message):
 
 	debug_print("message received: " + message.content)
 
-
 	if ('<@!%s>' % bot.user.id) in message.content or ('<@%s>' % bot.user.id) in message.content:
-		await  message.channel.send(magic_eight_ball())
+		# test stuff
+		await message.channel.send(parse_str_emoji("___EMOJI_CRY___ er is iets ___EMOJI_CRY___ niet goed gegaan ___EMOJI_CRY___"))
+		#await message.channel.send(parse_str_emoji(magic_eight_ball()))
 
 	elif randrange(0, 60) == 5:
 		await message.channel.send(get_random_quote())
--- src/helpers.py
+++ src/helpers.py
@@ -1,8 +1,9 @@
-import datetime, os
+import datetime, os, json
 from random import randrange
 from dotenv import load_dotenv
 
 dot_env_loaded = False
+texts = None
 debug_bool = True
 
 def random_datetime_in_range(start, end):
@@ -25,3 +26,25 @@ def debug_print(input):
 		else:
 			print("<DEBUG> ")
 			print(input)
+
+def get_text(*args):
+    global texts
+    if not texts:
+        with open(env('TEXTS_FILE')) as f:
+            texts = json.load(f)
+    
+    if len(args) == 0:
+        raise (ValueError("get_text called without params"))
+    
+    result = ""
+    try:
+        result = texts[args[0]]
+        
+        for i in range(1, len(args)):
+            result = result[args[i]]
+    except KeyError as e:
+        print("text not found, keys: " + str(args))
+        return ""
+
+    return result
+
